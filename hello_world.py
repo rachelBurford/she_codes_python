450e026f54b9ecfaecec91bf5902466be13d35e5
rachelBurford <rachel@pilbaratax.com.au>
2021-06-07 19:52:03 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+from os import O_NDELAY
+
+
+print("Hello, world!")
+
+day = "Monday"
+month = "June"
+print(day)
+print(f"Today is {day} and the month is {month}")
+
+run1_distance = 1400
+print(run1_distance)
+print(type(run1_distance))
+
+run2_distance = 1800
+
+total_distance = run1_distance + run2_distance
+difference = run2_distance - run1_distance
+print(total_distance)
+print(difference)
+
+run3_split = 1.3
+
+run4_split = 1.35
+print(type(run4_split))
+print(run1_distance / 1000)
+print(run4_split * 1000)
