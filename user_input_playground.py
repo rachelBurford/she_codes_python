eb7911e93ed39d0dd1f3119f4383ccb679eed095
rachelBurford <rachel@pilbaratax.com.au>
2021-06-08 17:48:14 +0800

    Create user_input_playground.py
    practice using strings, integers and floats

---
+++ user_input_playground.py
@@ -0,0 +1,6 @@
+count = input("Enter a number")
+count1 = (float(count))
+count2 = input("Enter another Number")
+count2_2 = (float(count2))
+addition = (count1) * (count2_2)
+print(f"did you know {count1} times {count2_2} = {addition}?")
