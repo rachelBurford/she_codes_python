ae758fd3ebe7357b414de8ccae82f96ae0e56d85
rachelBurford <rachel@pilbaratax.com.au>
2021-06-08 18:00:58 +0800

    Update user_input_playground.py

--- user_input_playground.py
+++ user_input_playground.py
@@ -1,6 +1,15 @@
-count = input("Enter a number")
-count1 = (float(count))
-count2 = input("Enter another Number")
-count2_2 = (float(count2))
-addition = (count1) * (count2_2)
-print(f"did you know {count1} times {count2_2} = {addition}?")
+#count = input("Enter a number")
+#count1 = (float(count))
+#count2 = input("Enter another Number")
+#count2_2 = (float(count2))
+#addition = (count1) * (count2_2)
+#print(f"did you know {count1} times {count2_2} = {addition}?")
+
+#kms = input("how many KM's did you go?")
+#kms1 = (float(kms))
+#kms10 = (int(kms1 * 1000))
+#kms100 = (int(kms10 * 100))
+#print(f"did you know that is {kms10}m")
+#print(f"Or .. its {kms100}cm!!")
+
+# interchange the int and float in the lines to get the output to have . or not
